41a26b5c6144a05487c6a3e42737957cda3b1f77
Gustav <119504010+Gustav6@users.noreply.github.com>
2024-05-06 10:33:41 +0200

    Start on project

---
+++ AStar/Game1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace AStar
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D tileTexture;
+        private Tile[,] tileMap;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            tileMap = new Tile[20, 10];
+
+            for (int x = 0; x < tileMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < tileMap.GetLength(1); y++)
+                {
+                    tileMap[x, y] = new Tile(tileTexture, new Vector2(x * tileTexture.Width, y * tileTexture.Height));
+                }
+            }
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            tileTexture = CreateTexture(32, 32, pixel => Color.White);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+        private Texture2D CreateTexture(int width, int height, Func<int, Color> paint)
+        {
+            Texture2D texture = new(GraphicsDevice, width, height);
+
+            Color[] colorArray = new Color[width * height];
+
+            for (int pixel = 0; pixel < colorArray.Length; pixel++)
+            {
+                colorArray[pixel] = paint(pixel);
+            }
+
+            texture.SetData(colorArray);
+
+            return texture;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            for (int x = 0; x < tileMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < tileMap.GetLength(1); y++)
+                {
+                    tileMap[x, y].Draw(_spriteBatch);
+                }
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ AStar/Tile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStar
+{
+    public class Tile
+    {
+        Texture2D texture;
+        Vector2 position;
+
+        public Tile(Texture2D _texture, Vector2 _position)
+        {
+            texture = _texture;
+            position = _position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
